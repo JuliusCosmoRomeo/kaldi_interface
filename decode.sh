cd4c14dc5421a83611a80d6934c3193475bec27a
Julius Rudolph <julius.rudolph@student.hpi.de>
2017-08-01 12:55:57 +0000

    automatically generating test files now

--- decode.sh
+++ decode.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 if [ $1 = "--help" ]
 then
-  echo "Expected format: ./train modelname data-dir"
+  echo "Expected format: ./decode.sh modelname test-dir"
   exit
 fi
 
@@ -18,8 +18,31 @@ else
     echo "~/kaldi/egs/$1/s5"
     if [ -f ~/kaldi/egs/$1/s5/decode.sh ]
     then 
+       text_file_exists=0
+       if [ -f text ]
+       then
+         text_file_exists=1
+       fi
+       for file in $( find $2 -name "*.wav" ); do
+         filename=${file%.wav}
+         if ! grep -Fq "${filename##*/}" $2/wav.scp
+         then 
+           echo "${filename##*/} data/$1/${file##*/}" >> $2/wav.scp
+           echo "Wrote to wav.scp"
+         fi
+         if ! grep -Fq "${filename##*/}" $2/utt2spk
+         then
+           echo "${filename##*/} ${filename##*/}" >> $2/utt2spk
+           echo "Wrote to utt2spk"
+         fi
+         if ! grep -Fq "${filename##*/}" $2/text 
+         then 
+           echo "${filename##*/} None" >> $2/text
+           echo "Wrote to text"
+         fi
+       done
        cd ~/kaldi/egs/$1/s5/
-       ./decode.sh $2
+       #./decode.sh $2
     fi
   fi
 fi
