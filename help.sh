b77d49790a617579c9c38948421bfe4371a0af3d
Julius Rudolph <julius.rudolph@student.hpi.de>
2017-08-03 09:19:11 +0000

    added correct path also in the help.sh

--- help.sh
+++ help.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Your available commands are:" 
-echo "./train.sh modelname //expects data to be located under /data/models/modelname/wav"
-echo "./train.sh modelname data-dir [utterance-postfix]"
-echo "./decode.sh modelname test-dir"
+echo "/kaldi_interface/train.sh modelname //expects data to be located under /data/models/modelname/wav"
+echo "/kaldi_interface/train.sh modelname data-dir [utterance-postfix]"
+echo "/kaldi_interface/decode.sh modelname test-dir"
 echo "_______________________________________________________________________________"
 echo "Available utterance-postfixes are:"
 echo "_Kinect-Beam"
